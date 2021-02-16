cadab6d83c70d2e5daab802b92d2c9f3a2ab4c9e
Alex Berumen <alexandroberumen@gmail.com>
2021-02-16 12:54:54 -0800

    real estate finished

---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from '../images/ImgOne.jpg'
+import ImageTwo from '../images/ImgTwo.jpg'
+
+
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis autem obcaecati dignissimos quisquam labore veniam. Quibusdam minus architecto at a ratione reiciendis itaque excepturi odio.',
+    paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, sit.',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100,
+
+}
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciaos quisquam labore veniam. Quibusdam minus architecto at a ratione reiciendis itaque excepturi odio.',
+    paragraphTwo: 'Lorem ipsum dolor sit ametctetur adipisicing elit. Inventore, sit.',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300,
+
+}
